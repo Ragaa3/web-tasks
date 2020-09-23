8496a04e070d4fc345552e537cfe5b328af7bf80
Ragaa3 <69164755+Ragaa3@users.noreply.github.com>
2020-09-23 14:20:16 +0200

    my project

---
+++ project-9-html5/js/file.js
@@ -0,0 +1,7 @@
+var mycanvas = document.getElementById('c'),
+    mycontext = mycanvas.getContext('2d');
+mycontext.font ='30px arial';
+mycontext.fillStyle = '#1e90ff';
+mycontext.strokeStyle='pink';
+mycontext.fillText('Almass & jewellery',20,80);
+mycontext.strokeText('Almass & jewellery',20,80);
