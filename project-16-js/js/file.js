19f1e58264645d82f5bf595fac1d649e2011538b
Ragaa3 <69164755+Ragaa3@users.noreply.github.com>
2020-11-05 23:37:23 +0200

    my project

---
+++ project-16-js/js/file.js
@@ -0,0 +1,26 @@
+function malti()
+{
+    var number = document.getElementById("n1").value;
+    document.getElementById("text1").innerHTML="plz wait 3 second";
+    document.getElementById("result").innerHTML="";
+    for(var i=0;i<=12;i=i+1)
+    {
+        var m = number*i;
+        document.getElementById("result1").innerHTML+=number+" * "+i+" = "+m+"<br>";
+    }
+}
+function clean()
+{
+    document.getElementById("result2").innerHTML="";
+}
+function repeat()
+{
+    var number = document.getElementById("n2").value;
+    var i = number;
+    do 
+    {
+        document.getElementById("result2").innerHTML+="repeat "+number+" time "+"<br>";
+        i--;
+    }
+    while(i!=0);
+}
