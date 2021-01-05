73b28ccd2608bba3a4a6443112a4785123d634a6
Ragaa3 <69164755+Ragaa3@users.noreply.github.com>
2021-01-05 20:02:35 +0200

    my project

--- my-task/js/file.js
+++ my-task/js/file.js
@@ -16,76 +16,88 @@ var inpu = document.getElementById("input").value,
     bdi= document.getElementById("divs").value,
     bpri= document.getElementById("btpri").value,
     bd= document.getElementById("btd").value;
+// button one
 function conctbt1()
 {
     "use strict";
     inpu +=b1;
     document.getElementById("input").value=inpu;
 }
+// button two
 function conctbt2()
 {
     "use strict";
     inpu +=b2;
     document.getElementById("input").value=inpu
 }
+// button three
 function conctbt3()
 {
     "use strict";
     inpu +=b3;
     document.getElementById("input").value=inpu
 }
+// button four
 function conctbt4()
 {
     "use strict";
     inpu +=b4;
     document.getElementById("input").value=inpu
 }
+// button five
 function conctbt5()
 {
     "use strict";
     inpu +=b5;
     document.getElementById("input").value=inpu
 }
+// button six
 function conctbt6()
 {
     "use strict";
     inpu +=b6;
     document.getElementById("input").value=inpu
 }
+// button seven
 function conctbt7()
 {
     "use strict";
     inpu +=b7;
     document.getElementById("input").value=inpu
 }
+// button eight
 function conctbt8()
 {
     "use strict";
     inpu +=b8;
     document.getElementById("input").value=inpu
 }
+// button nine
 function conctbt9()
 {
     "use strict";
     inpu +=b9;
     document.getElementById("input").value=inpu
 }
+// button zero
 function conctbt0()
 {
     "use strict";
     inpu +=b0;
     document.getElementById("input").value=inpu
 }
+// button pluse +
 function conctbtpl()
 {
     "use strict";
     inpu +=plus;
     document.getElementById("input").value=inpu
 }
+// button equal =
 function conctbteq()
 {
     "use strict";
-    if(eval(inpu) == undefined)
+    if(eval(inpu) == undefined) // if input empty
     {
         document.getElementById("input").value=" please enter number";
     }
@@ -103,42 +115,49 @@ function conctbteq()
     }
     inpu='';
 }
+// button minuse -
 function conctbtmi()
 {
     "use strict";
     inpu +=bmi;
     document.getElementById("input").value=inpu
 }
+// button mlutiplier x
 function conctbtml()
 {
     "use strict";
     inpu +=bml;
     document.getElementById("input").value=inpu;
 }
+// button division /
 function conctbtdiv()
 {
     "use strict";
     inpu +=bdi;
     document.getElementById("input").value=inpu
 }
+// button mode %
 function conctbtp()
 {
     "use strict";
     inpu +=bpri;
     document.getElementById("input").value=inpu
 }
+// button dot .
 function conctbtd()
 {
     "use strict";
     inpu +=bd;
     document.getElementById("input").value=inpu
 }
+// button clear
 function clear()
 {
     "use strict";
     document.getElementById("input").value=inpu.replace(inpu,'');
     
 }
+// button delete
 function conctbtdel()
 {
     "use strict";
