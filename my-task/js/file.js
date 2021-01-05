1e90fbd52c3728c9c9019a8eea6bd4c264bd5b2e
Ragaa3 <69164755+Ragaa3@users.noreply.github.com>
2021-01-05 22:50:02 +0200

    my project

--- my-task/js/file.js
+++ my-task/js/file.js
@@ -20,156 +20,216 @@ var inpu = document.getElementById("input").value,
 function conctbt1()
 {
     "use strict";
-    inpu +=b1;
-    document.getElementById("input").value=inpu;
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b1;
+        document.getElementById("input").value=inpu;
+    }
 }
 // button two
 function conctbt2()
 {
     "use strict";
-    inpu +=b2;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b2;
+        document.getElementById("input").value=inpu
+    }
 }
 // button three
 function conctbt3()
 {
     "use strict";
-    inpu +=b3;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b3;
+        document.getElementById("input").value=inpu
+    }
 }
 // button four
 function conctbt4()
 {
     "use strict";
-    inpu +=b4;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b4;
+        document.getElementById("input").value=inpu
+    }
 }
 // button five
 function conctbt5()
 {
     "use strict";
-    inpu +=b5;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b5;
+        document.getElementById("input").value=inpu
+    }
 }
 // button six
 function conctbt6()
 {
     "use strict";
-    inpu +=b6;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b6;
+        document.getElementById("input").value=inpu
+    }
 }
 // button seven
 function conctbt7()
 {
     "use strict";
-    inpu +=b7;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b7;
+        document.getElementById("input").value=inpu
+    }
 }
 // button eight
 function conctbt8()
 {
     "use strict";
-    inpu +=b8;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b8;
+        document.getElementById("input").value=inpu
+    }
 }
 // button nine
 function conctbt9()
 {
     "use strict";
-    inpu +=b9;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b9;
+        document.getElementById("input").value=inpu
+    }
 }
 // button zero
 function conctbt0()
 {
     "use strict";
-    inpu +=b0;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=b0;
+        document.getElementById("input").value=inpu
+    }
 }
 // button pluse +
 function conctbtpl()
 {
     "use strict";
-    inpu +=plus;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=plus;
+        document.getElementById("input").value=inpu
+    }
 }
 // button equal =
 function conctbteq()
 {
     "use strict";
-    if(eval(inpu) == undefined) // if input empty
+    if (document.getElementById("input").readOnly == false)
     {
-        document.getElementById("input").value=" please enter number";
-    }
-    else
-    {
-        if(isFinite(eval(inpu)) == false)
+        if(eval(inpu) == undefined) // if input empty
         {
-            document.getElementById("input").value=" Error is not a number ";  
+            document.getElementById("input").value=" please enter number";
         }
         else
         {
-            var a=eval(inpu);
-            document.getElementById("input").value=a;
-        } 
-    }
+            if(isFinite(eval(inpu)) == false)
+            {
+                document.getElementById("input").value=" Error is not a number ";  
+            }
+            else
+            {
+                var a=eval(inpu);
+                document.getElementById("input").value=a;
+            } 
+        }
     inpu='';
+    }
 }
 // button minuse -
 function conctbtmi()
 {
     "use strict";
-    inpu +=bmi;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=bmi;
+        document.getElementById("input").value=inpu
+    }
 }
 // button mlutiplier x
 function conctbtml()
 {
     "use strict";
-    inpu +=bml;
-    document.getElementById("input").value=inpu;
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=bml;
+        document.getElementById("input").value=inpu
+    }
 }
 // button division /
 function conctbtdiv()
 {
     "use strict";
-    inpu +=bdi;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=bdi;
+        document.getElementById("input").value=inpu
+    }
 }
 // button mode %
 function conctbtp()
 {
     "use strict";
-    inpu +=bpri;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=bpri;
+        document.getElementById("input").value=inpu
+    }
 }
 // button dot .
 function conctbtd()
 {
     "use strict";
-    inpu +=bd;
-    document.getElementById("input").value=inpu
+    if (document.getElementById("input").readOnly == false)
+    {
+        inpu +=bd;
+        document.getElementById("input").value=inpu
+    }
 }
 // button clear
 function clean()
 {
     "use strict";
-    document.getElementById("input").value='';
-    document.getElementById("input").placeholder='';
-    inpu=''
+    if (document.getElementById("input").readOnly == false)
+    {
+        document.getElementById("input").value='';
+        document.getElementById("input").placeholder='';
+        inpu=''
+    }
+    
 }
 // button on
 function on()
 {
     "use strict";
+    document.getElementById("input").value='';
     document.getElementById("input").placeholder='';
+    document.getElementById("input").readOnly = false;
     inpu=''
 }
+// button off
 function of()
 {
     "use strict";
+    document.getElementById("input").value='';
     document.getElementById("input").placeholder='CASIO';
-    inpu=''
+    document.getElementById("input").readOnly = true;
+    inpu='';
 }
 // button delete
 function conctbtdel()
