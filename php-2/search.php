232ec4fd9c701d9f96887a53902a401fad4413a5
Ragaa3 <69164755+Ragaa3@users.noreply.github.com>
2021-08-22 14:14:58 +0200

    my projet

--- php-2/search.php
+++ php-2/search.php
@@ -39,20 +39,19 @@ for ( $i=0;$i<3;$i++ )
             <?php 
     $res = $_GET['res'];
     $flowers = array( 'ahmed' , 'ali' , 'alaa' );
-        if ($class == 'sucess';)
+        if ($class == 'sucess')
         {
-            echo "<div class='alert  mt-5 alert-sucess' role='alert'>".
+            echo "<div class='alert  mt-5 alert-success' role='alert'>".
                 "<h4 class='alert-heading'>"."Well done!</h4>".
                 "<p>"."Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content."."</p>"
-                ."<hr>"
+                ."<hr>".
                 "<p class='mb-0'>"."Whenever you need to, be sure to use margin utilities to keep things nice and tidy."."</p>"
             ."</div>";
-            break;
         }
         else
         {
             echo "<div class='alert  mt-5 alert-danger' role='alert'>".
-                "<h4 class='alert-heading'>"."Well done!"."</h4>".
+                "<h4 class='alert-heading'>"."Waring this!"."</h4>".
                 "<p>"."Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content."."</p>".
                 "<hr>".
                 "<p class='mb-0'>"."Whenever you need to, be sure to use margin utilities to keep things nice and tidy."."</p>"
